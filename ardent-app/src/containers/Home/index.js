777226509388b6a35e829e9c8bd6fc2856f5b437
Suit <suit387@gmail.com>
2017-07-27 10:25:56 -0700

    Update

--- ardent-app/src/containers/Home/index.js
+++ ardent-app/src/containers/Home/index.js
@@ -1,3 +1,29 @@
 import React from "react";
 
-export default () => <h1>Home!</h1>;
+import Container from "../../components/Container";
+
+import { Hero, Robot, HeroText, HeroButton, HeroDiscoverMore } from "./styles";
+
+import robotImage from "./robot.png";
+import downArrowIcon from "./down-arrow.svg";
+
+export default () =>
+  <Hero>
+    <Container>
+      <Robot>
+        <img src={robotImage} alt="Ardent Robot" />
+      </Robot>
+      <HeroText>AN ALL-PURPOSE, MULTILINGUAL AND FUN DISCORD BOT</HeroText>
+      <div className="hero-divider" />
+      <div className="hero-buttons">
+        <HeroButton>Dashboard</HeroButton>
+        <HeroButton>Invite to Discord</HeroButton>
+        <HeroButton>Web Console</HeroButton>
+      </div>
+      <HeroDiscoverMore>
+        <span>discover more</span>
+        <br />
+        <img src={downArrowIcon} />
+      </HeroDiscoverMore>
+    </Container>
+  </Hero>;
---
+++ ardent-app/src/containers/Home/styles.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+import BG from "./background.jpg";
+
+export const Hero = styled.div`
+  height: 400px;
+  background: no-repeat top url(${BG});
+  background-size: cover;
+  padding: 120px 0px;
+  > section > .hero-divider {
+    height: 2px;
+    width: 80%;
+    background-color: #e7eaef;
+    margin: auto;
+  }
+  > section > .hero-buttons {
+    text-align: center;
+    margin-top: 40px;
+  }
+`;
+
+export const Robot = styled.div`
+  > img {
+    display: block;
+    margin: auto;
+    height: 142px;
+  }
+  margin-bottom: 40px;
+`;
+
+export const HeroText = styled.div`
+  text-align: center;
+  font-weight: 700;
+  font-size: 24px;
+  width: 50%;
+  margin: auto;
+  color: #2b3e4e;
+  letter-spacing: 5px;
+  margin-bottom: 40px;
+`;
+
+export const HeroButton = styled.button`
+  background: none;
+  border: 2px #a8b9c0 solid;
+  border-radius: 22px;
+  padding: 12px 32px;
+  margin: 0 30px;
+  color: #5d7183;
+  font-weight: 600;
+  letter-spacing: 2px;
+  font-size: 12px;
+`;
+
+export const HeroDiscoverMore = styled.div`
+  text-align: center;
+  margin-top: 80px;
+  > img {
+    height: 16px;
+    margin: auto;
+  }
+  > span {
+    font-size: 12px;
+    color: #bbbec3;
+  }
+`;
