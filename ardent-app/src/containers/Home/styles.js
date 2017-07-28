8904ff4ac37f2c319e73e32af2bba7be8e563372
adugar <armaandugar123@gmail.com>
2017-07-28 17:09:33 -0400

    changed height

--- ardent-app/src/containers/Home/styles.js
+++ ardent-app/src/containers/Home/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import BG from "./background.jpg";
 
 export const Hero = styled.div`
-  height: 600px;
+  height: 700px;
   background: no-repeat top url(${BG});
   background-size: cover;
   padding: 120px 0px;
